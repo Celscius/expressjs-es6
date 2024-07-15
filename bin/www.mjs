d152ed39038f1f47b227ab9bcb8fcec574ea3726
Dcw0818 <60767284+Celscius@users.noreply.github.com>
2024-07-15 12:58:13 +0800

    add listening port on console

--- bin/www.mjs
+++ bin/www.mjs
@@ -22,15 +22,40 @@ app.set('port', port);
  * Create HTTP server.
  */
 
+/*
+  get ip address
+  using require("os")
+  get local address from network interface
+  get networkInterfaces()
+  networkInterfaces["Wi-Fi"][1].address //get address 192.168.1.4
+  networkInterfaces["Loopback Pseudo-Interface 1"][1].address //get address 127.0.0.1
+  os.hostname(); //get host name e.g collasour
+*/
+
+import os from 'os'
+const networkInterfaces = os.networkInterfaces();
+const wifi = "Wi-Fi"
+const loopback = "Loopback Pseudo-Interface 1"
+const address_wifi = networkInterfaces[wifi][1].address 
+const address_loopback = networkInterfaces[loopback][1].address
+const localhost = 'localhost'  
+/**
+ * Create HTTP server.
+ */
+
 var server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
  */
-
-server.listen(port);
+ 
+app.listen(port,address_wifi,localhost, () => {
+  console.log(`> listening on port ${address_wifi}:${port}\n`)
+  //console.log(`> listening on port ${address_loopback}:${port}`)
+})
+//server.listen(port);
 server.on('error', onError);
-server.on('listening', onListening);
+server.on('listening', onListening,);
 
 /**
  * Normalize a port into a number, string, or false.
